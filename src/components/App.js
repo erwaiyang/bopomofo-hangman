86307f1d1fb53174485dffd4787cfafbf6c5932a
Jean Yang <bdlas321@gmail.com>
2017-03-20 18:06:23 +0800

    semantic ui integration

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Header, Button } from 'semantic-ui-react';
 import { GET_STARTED, YOU_WIN, YOU_LOST } from '../constants/actionTypes';
 import Game from './Game';
 
 function App({ status }) {
   return (
     <div id="app">
-      <h1>注音吊死鬼</h1>
+      <Header as="h1">注音吊死鬼</Header>
       {
         status === 'INIT' &&
-          <button>開始遊戲</button>
+          <Button primary>開始遊戲</Button>
         ||
         status === GET_STARTED &&
           <Game />
