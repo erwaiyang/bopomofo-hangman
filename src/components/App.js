e8a7826d3a7052b2bd123c4ab4ea7ef2114b5f42
Jean Yang <bdlas321@gmail.com>
2017-03-20 18:13:13 +0800

    semantic ui Container

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Header, Button } from 'semantic-ui-react';
+import { Container, Header, Button } from 'semantic-ui-react';
 import { GET_STARTED, YOU_WIN, YOU_LOST } from '../constants/actionTypes';
 import { getStarted } from '../actions/gameActions';
 import Game from './Game';
@@ -9,14 +9,16 @@ function App(props) {
   const { status } = props;
   return (
     <div id="app">
-      <Header as="h1">注音吊死鬼</Header>
-      {
-        status === 'INIT' &&
-          <Button primary onClick={() => props.getStarted()}>開始遊戲</Button>
-        ||
-        status === GET_STARTED &&
-          <Game />
-      }
+      <Container>
+        <Header as="h1">注音吊死鬼</Header>
+        {
+          status === 'INIT' &&
+            <Button primary onClick={() => props.getStarted()}>開始遊戲</Button>
+          ||
+          status === GET_STARTED &&
+            <Game />
+        }
+      </Container>
     </div>
   );
 }
