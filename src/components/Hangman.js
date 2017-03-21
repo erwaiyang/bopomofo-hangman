5a7e5763d497bd11d5c101b8578f94d59f50643e
Jean Yang <bdlas321@gmail.com>
2017-03-21 14:51:27 +0800

    use Message component to show all guessed

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,13 +1,15 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Statistic, Message } from 'semantic-ui-react';
+import { Container, Statistic, Message } from 'semantic-ui-react';
 import { MAX_STEP } from '../constants/parameters';
 
 function Hangman({ step, guessed }) {
   return (
     <div id="hangman">
-      <Statistic value={`${step} / ${MAX_STEP}`} label="機會" />
-      <Message info>已猜：{guessed.toString()}</Message>
+      <Container>
+        <Statistic value={`${step} / ${MAX_STEP}`} label="機會" />
+        <Message info>已猜：{guessed.toString()}</Message>
+      </Container>
     </div>
   );
 }
