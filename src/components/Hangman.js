bd2ea4c7053d737ac835b10463fd32ae3f6b6fd9
Jean Yang <bdlas321@gmail.com>
2017-03-21 14:46:46 +0800

    step status

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Statistic } from 'semantic-ui-react';
 import { MAX_STEP } from '../constants/parameters';
 
 function Hangman({ step, guessed }) {
   return (
     <div id="hangman">
-      <p>{`${step} / ${MAX_STEP}`}</p>
+      <Statistic value={`${step} / ${MAX_STEP}`} label="機會" />
       <p>已猜：{guessed.toString()}</p>
     </div>
   );
