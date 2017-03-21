8ea3a4456e505a744a4ec77f639084ab7c46dca7
Jean Yang <bdlas321@gmail.com>
2017-03-21 11:17:00 +0800

    disable input

--- src/components/BopomofoFrame.js
+++ src/components/BopomofoFrame.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 
 const BopomofoFrame = ({ b }) => (
   <div>
-    <input type="text" value={b} size={2} />
+    <input type="text" value={b} size={2} disabled />
   </div>
 );
 
