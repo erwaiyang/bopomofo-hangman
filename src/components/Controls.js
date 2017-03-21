375432d5cddb0bc1537f7eb661dcb49185f7b7d2
Jean Yang <bdlas321@gmail.com>
2017-03-21 14:18:46 +0800

    auto clear input value after user fires onGuessClick

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,20 +1,32 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Input, Button } from 'semantic-ui-react';
 import { guess } from '../actions/gameActions';
 
-function Controls(props) {
-  this.onGuessChange = (e) => {
-    this.b = e.target.value;
+class Controls extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { guess: '', answer: '' };
+  }
+  onGuessChange = (e) => {
+    this.setState({ guess: e.target.value });
   };
-  return (
-    <div id="controls">
-      <Input label="注音" placeholder="eg. ㄅ,ㄆ,ˊ（二聲）" onChange={this.onGuessChange} />
-      <Button primary onClick={() => props.guess(this.b)}>猜注音</Button>
-      <Input label="答案" placeholder="eg. 哈囉世界" />
-      <Button secondary>直接猜！</Button>
-    </div>
-  );
+  onGuessClick = () => {
+    if (this.state.guess !== '') {
+      this.props.guess(this.state.guess);
+      this.setState({ guess: '' });
+    }
+  };
+  render() {
+    return (
+      <div id="controls">
+        <Input label="注音" placeholder="eg. ㄅ,ㄆ,ˊ（二聲）" value={this.state.guess} onChange={this.onGuessChange} />
+        <Button primary onClick={this.onGuessClick}>猜注音</Button>
+        <Input label="答案" placeholder="eg. 哈囉世界" />
+        <Button secondary>直接猜！</Button>
+      </div>
+    );
+  }
 }
 
 Controls.propTypes = {
