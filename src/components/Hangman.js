bafab9ca245e36a15314e9234460cd2e2e725e46
Jean Yang <bdlas321@gmail.com>
2017-03-21 11:46:53 +0800

    incorrect guessed bopomofos

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -2,20 +2,23 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { MAX_STEP } from '../constants/parameters';
 
-function Hangman({ step }) {
+function Hangman({ step, incorrect }) {
   return (
     <div id="hangman">
       <p>{`${step} / ${MAX_STEP}`}</p>
+      <p>已猜：{incorrect.toString()}</p>
     </div>
   );
 }
 
 Hangman.propTypes = {
+  incorrect: PropTypes.arrayOf(PropTypes.string).isRequired,
   step: PropTypes.number.isRequired,
 };
 
-function mapStateToProps({ gameStore: { step } }) {
+function mapStateToProps({ gameStore: { step, incorrect } }) {
   return {
+    incorrect,
     step,
   };
 }
